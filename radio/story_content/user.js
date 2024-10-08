8871fd99892fd2e455b135c7de0c34f823baa485
Patrick Murphy <patrick.murphy@mailchimp.com>
2024-10-08 00:14:13 -0400

    Update Radio Training

--- radio/story_content/user.js
+++ radio/story_content/user.js
@@ -1,39 +1,8 @@
-function ExecuteScript(strId)
-{
-  switch (strId)
-  {
-      case "6IKzkUaGcZR":
-        Script1();
-        break;
-  }
-}
-
-window.InitExecuteScripts = function()
+window.InitUserScripts = function()
 {
 var player = GetPlayer();
 var object = player.object;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
-window.Script1 = function()
-{
-  player.once(() => {
-const target = object('6pIr16X0Hct');
-const duration = 750;
-const easing = 'ease-out';
-const id = '6F2rQSNLVdA';
-const pulseAmount = 0.07;
-const delay = 0;
-addToTimeline(
-target.animate([
-{ scale: '1' }, { scale: `${1 + pulseAmount}` },
-{ scale: '1' }, { scale: `${1 + pulseAmount}` },
-{ scale: '1' }
-],
-  { fill: 'forwards', delay, duration, easing }
-), id
-);
-});
-}
-
 };
